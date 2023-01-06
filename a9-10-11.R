8f95820b7565f86a102ff6740ed3c742bb9e89bd
diegocostam <diegocostta95@hotmail.com>
2023-01-06 12:27:32 -0300

    EMERGENCIA

--- a9-10-11.R
+++ a9-10-11.R
@@ -29,22 +29,22 @@ add_grupo_nosologia <- function(vetor){
 # Criando um laço para compilar os Anexos mensais em um único projeto
 for (i in 1:length(data_names)) {
   sheet9_1 <- read_excel(data_names[i], sheet = 9, range = "A4:G45", col_names = FALSE) %>% 
-    select(!c(4, 6)) %>% rename(`GRUPOS DE DOENÇAS` = `...1`, CID = `...2`, AMBULATORIAL = `...3`, `EMERGÊNCIAL` = `...5`, `INTERNAÇÃO` = `...7`) %>% filter(!is.na(CID))
+    select(!c(4, 6)) %>% rename(`GRUPOS DE DOENÇAS` = `...1`, CID = `...2`, AMBULATORIAL = `...3`, `EMERGÊNCIA` = `...5`, `INTERNAÇÃO` = `...7`) %>% filter(!is.na(CID))
   
   sheet9_2 <- read_excel(data_names[i], sheet = 9, range = "J4:P38", col_names = FALSE) %>% 
-    select(!c(4, 6)) %>% rename(`GRUPOS DE DOENÇAS` = `...1`, CID = `...2`, AMBULATORIAL = `...3`, `EMERGÊNCIAL` = `...5`, `INTERNAÇÃO` = `...7`) %>% filter(!is.na(CID))
+    select(!c(4, 6)) %>% rename(`GRUPOS DE DOENÇAS` = `...1`, CID = `...2`, AMBULATORIAL = `...3`, `EMERGÊNCIA` = `...5`, `INTERNAÇÃO` = `...7`) %>% filter(!is.na(CID))
   
   sheet10_1 <- read_excel(data_names[i], sheet = 10, range = "A4:G40", col_names = FALSE) %>% 
-    select(!c(4, 6)) %>% rename(`GRUPOS DE DOENÇAS` = `...1`, CID = `...2`, AMBULATORIAL = `...3`, `EMERGÊNCIAL` = `...5`, `INTERNAÇÃO` = `...7`) %>% filter(!is.na(CID))
+    select(!c(4, 6)) %>% rename(`GRUPOS DE DOENÇAS` = `...1`, CID = `...2`, AMBULATORIAL = `...3`, `EMERGÊNCIA` = `...5`, `INTERNAÇÃO` = `...7`) %>% filter(!is.na(CID))
   
   sheet10_2 <- read_excel(data_names[i], sheet = 10, range = "J4:P39", col_names = FALSE) %>% 
-    select(!c(4, 6)) %>% rename(`GRUPOS DE DOENÇAS` = `...1`, CID = `...2`, AMBULATORIAL = `...3`, `EMERGÊNCIAL` = `...5`, `INTERNAÇÃO` = `...7`) %>% filter(!is.na(CID))
+    select(!c(4, 6)) %>% rename(`GRUPOS DE DOENÇAS` = `...1`, CID = `...2`, AMBULATORIAL = `...3`, `EMERGÊNCIA` = `...5`, `INTERNAÇÃO` = `...7`) %>% filter(!is.na(CID))
   
   sheet11_1 <- read_excel(data_names[i], sheet = 11, range = "A4:G39", col_names = FALSE) %>% 
-    select(!c(4, 6)) %>% rename(`GRUPOS DE DOENÇAS` = `...1`, CID = `...2`, AMBULATORIAL = `...3`, `EMERGÊNCIAL` = `...5`, `INTERNAÇÃO` = `...7`) %>% filter(!is.na(CID))
+    select(!c(4, 6)) %>% rename(`GRUPOS DE DOENÇAS` = `...1`, CID = `...2`, AMBULATORIAL = `...3`, `EMERGÊNCIA` = `...5`, `INTERNAÇÃO` = `...7`) %>% filter(!is.na(CID))
   
   sheet11_2 <- read_excel(data_names[i], sheet = 11, range = "J4:P42", col_names = FALSE) %>% 
-    select(!c(4, 6)) %>% rename(`GRUPOS DE DOENÇAS` = `...1`, CID = `...2`, AMBULATORIAL = `...3`, `EMERGÊNCIAL` = `...5`, `INTERNAÇÃO` = `...7`) %>% filter(!is.na(CID))
+    select(!c(4, 6)) %>% rename(`GRUPOS DE DOENÇAS` = `...1`, CID = `...2`, AMBULATORIAL = `...3`, `EMERGÊNCIA` = `...5`, `INTERNAÇÃO` = `...7`) %>% filter(!is.na(CID))
   
   data <- bind_rows(sheet9_1, sheet9_2, sheet10_1, sheet10_2, sheet11_1, sheet11_2) %>% mutate(`GRUPOS DE DOENÇAS` = add_grupo_nosologia(`GRUPOS DE DOENÇAS`))
   
